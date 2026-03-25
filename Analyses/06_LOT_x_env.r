389b887e60c1a7df776b8ba0748c04520216b8c8
Mathys Dussel <106338396+Mathys-Dussel@users.noreply.github.com>
2026-03-25 10:08:27 +0100

    utilisation des données du LOT1 pour corrélation avec données environnementales

--- Analyses/06_LOT_x_env.r
+++ Analyses/06_LOT_x_env.r
@@ -85,6 +85,158 @@ ZONES_LOT_123$LOT.SampleCode  <- gsub("/.*", "", ZONES_LOT_123$LOT.SampleCode)
 
 
 
+
+
+
+
+
+
+
+
+
+library(phyloseq)
+library(ggplot2)
+library(ggpubr)
+library(patchwork)
+
+ps <- readRDS("donnees/ps_final.rds")
+sample_names(ps) <- gsub("\\.", "-", sub("^X", "", sample_names(ps)))
+
+ps_LOT1 <- subset_samples(ps, startsWith(LOT_sampleID, "LOT01"))
+
+ps_LOT1 <- prune_taxa(taxa_sums(ps_LOT1) > 0, ps_LOT1)
+
+
+
+sample_data(ps_LOT1)$Zone <- "Unknown"
+sample_data(ps_LOT1)$Zone[substr(sample_data(ps_LOT1)$LOT_sampleID, 9, 9) == "1"] <- "Bottom"
+sample_data(ps_LOT1)$Zone[substr(sample_data(ps_LOT1)$LOT_sampleID, 9, 9) %in% c("2", "3")] <- "top_trunk"
+sample_data(ps_LOT1)$Zone[substr(sample_data(ps_LOT1)$LOT_sampleID, 9, 9) == "4"] <- "Heart"
+sample_data(ps_LOT1)$Zone[substr(sample_data(ps_LOT1)$LOT_sampleID, 9, 9) %in% c("5", "6")] <- "periph_canopy"
+
+ps_LOT1_zones <- subset_samples(ps_LOT1, Zone != "Unknown")
+richness <- estimate_richness(ps_LOT1_zones, measures = c("Observed", "Shannon"))
+meta_data <- sample_data(ps_LOT1_zones)
+meta_data$Observed <- richness$Observed
+meta_data$ExpShannon <- exp(richness$Shannon)
+
+df_richness <- data.frame(
+    Sample = rownames(meta_data),
+    Zone = meta_data$Zone,
+    Observed = meta_data$Observed,
+    ExpShannon = meta_data$ExpShannon
+)
+
+df_richness$Zone <- factor(df_richness$Zone, levels = c("Bottom", "top_trunk", "Heart", "periph_canopy"))
+
+p_richness <- ggboxplot(df_richness, x = "Zone", y = "Observed", fill = "Zone") +
+    stat_compare_means(comparisons = my_comparisons, label = "p.signif") +
+    ggtitle("Différences de Richesse Spécifique (Observed) entre les zones")
+
+p_expshannon <- ggboxplot(df_richness, x = "Zone", y = "ExpShannon", fill = "Zone") +
+    stat_compare_means(comparisons = my_comparisons, label = "p.signif") +
+    ggtitle("Différences de Diversité (ExpShannon) entre les zones")
+
+print(p_richness)
+print(p_expshannon)
+
+kruskal.test(Observed ~ Zone, data = df_richness)
+pairwise.wilcox.test(df_richness$Observed, df_richness$Zone, p.adjust.method = "fdr")
+
+kruskal.test(ExpShannon ~ Zone, data = df_richness)
+pairwise.wilcox.test(df_richness$ExpShannon, df_richness$Zone, p.adjust.method = "fdr")
+
+library(dplyr)
+
+mean_temp_zone <- MC_TEMP_long %>%
+    group_by(Zone) %>%
+    summarise(Mean_Temp = mean(Temperature, na.rm = TRUE))
+
+mean_humi_zone <- MC_HUMI_long %>%
+    group_by(Zone) %>%
+    summarise(Mean_Humi = mean(Humidity, na.rm = TRUE))
+
+df_richness <- merge(df_richness, mean_temp_zone, by = "Zone", all.x = TRUE)
+df_richness <- merge(df_richness, mean_humi_zone, by = "Zone", all.x = TRUE)
+
+p_corr_temp_obs <- ggscatter(df_richness, x = "Mean_Temp", y = "Observed",
+                         add = "reg.line", conf.int = TRUE, 
+                         cor.coef = TRUE, cor.method = "spearman",
+                         xlab = "Température moyenne de la zone", 
+                         ylab = "Richesse Spécifique (Observed)") +
+    ggtitle("Relation entre la Température et la Richesse (Observed)")
+
+p_corr_humi_obs <- ggscatter(df_richness, x = "Mean_Humi", y = "Observed",
+                         add = "reg.line", conf.int = TRUE, 
+                         cor.coef = TRUE, cor.method = "spearman",
+                         xlab = "Humidité moyenne de la zone", 
+                         ylab = "Richesse Spécifique (Observed)") +
+    ggtitle("Relation entre l'Humidité et la Richesse (Observed)")
+
+print(p_corr_temp_obs)
+print(p_corr_humi_obs)
+
+p_corr_temp_exp <- ggscatter(df_richness, x = "Mean_Temp", y = "ExpShannon",
+                             add = "reg.line", conf.int = TRUE, 
+                             cor.coef = TRUE, cor.method = "spearman",
+                             xlab = "Température moyenne de la zone", 
+                             ylab = "Diversité (ExpShannon)") +
+    ggtitle("Relation entre la Température et la Diversité (ExpShannon)")
+
+p_corr_humi_exp <- ggscatter(df_richness, x = "Mean_Humi", y = "ExpShannon",
+                             add = "reg.line", conf.int = TRUE, 
+                             cor.coef = TRUE, cor.method = "spearman",
+                             xlab = "Humidité moyenne de la zone", 
+                             ylab = "Diversité (ExpShannon)") +
+    ggtitle("Relation entre l'Humidité et la Diversité (ExpShannon)")
+
+print(p_corr_temp_exp)
+print(p_corr_humi_exp)
+
+
+library(MASS)
+
+glm_humi_temp_obs <- glm.nb(Observed ~ Mean_Humi * Mean_Temp, data = df_richness)
+summary(glm_humi_temp_obs)
+
+glm_humi_temp_exp <- glm.nb(ExpShannon ~ Mean_Humi * Mean_Temp, data = df_richness)
+summary(glm_humi_temp_exp)
+
+
+
+library(DHARMa)
+
+simulationOutput_humi_temp_obs <- simulateResiduals(fittedModel = glm_humi_temp_obs, plot = TRUE)
+testUniformity(simulationOutput_humi_temp_obs)
+testDispersion(simulationOutput_humi_temp_obs)
+testZeroInflation(simulationOutput_humi_temp_obs)
+
+simulationOutput_humi_temp_exp <- simulateResiduals(fittedModel = glm_humi_temp_exp, plot = TRUE)
+testUniformity(simulationOutput_humi_temp_exp)
+testDispersion(simulationOutput_humi_temp_exp)
+testZeroInflation(simulationOutput_humi_temp_exp)
+
+
+library(interactions)
+interact_plot(glm_humi_temp_obs, pred = Mean_Humi, modx = Mean_Temp)
+interact_plot(glm_humi_temp_exp, pred = Mean_Humi, modx = Mean_Temp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 library(phyloseq)
 library(ggplot2)
 library(ggpubr)
